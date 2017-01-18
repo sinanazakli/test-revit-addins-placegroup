9ff47e91781156d6b1f30af87dbdc784eb1a9cb7
sinanazakli <azakli.sinan@gmail.com>
2017-01-18 11:43:26 +0100

    ExceptionHandling for ESC

--- ClassLibrary2/Class1.cs
+++ ClassLibrary2/Class1.cs
@@ -20,39 +20,38 @@ namespace PlaceGroup
           ref string message,
           ElementSet elements)
         {
-            // Hello github
-            //Get application and document objects
-            UIApplication uiApp = commandData.Application;
-            Document doc = uiApp.ActiveUIDocument.Document;
-            if (doc == null)
-                return Result.Cancelled;
+            try
+            {
+                //Get application and document objects
+                UIApplication   uiApp       = commandData.Application;
+                Document        doc         = uiApp.ActiveUIDocument.Document;
 
-            //Define a Reference object to accept the pick result.
-            Reference pickedRef = null;
+                //Define a Reference object to accept the pick result.
+                // Reference       pickedRef   = null;
 
-            //Pick a group
-            Selection sel = uiApp.ActiveUIDocument.Selection;
-            if (sel == null)
-                return Result.Cancelled;
-            pickedRef = sel.PickObject(ObjectType.Element, "Please select a group");
-            if (pickedRef == null)
-                return Result.Cancelled;
-            Element elem = doc.GetElement(pickedRef);
-            if (elem == null)
-                return Result.Cancelled;
-            Group group = elem as Group;
+                //Pick a group
+                Selection       sel         = uiApp.ActiveUIDocument.Selection;
+                Reference       pickedRef   = sel.PickObject(ObjectType.Element, "Please select a group");
+                Element         elem        = doc.GetElement(pickedRef);
+                Group           group       = elem as Group;
 
-            //Pick a point
-            XYZ point = sel.PickPoint("Please pick a point to place group");
-            if (point == null)
+                //Pick a point
+                XYZ             point       = sel.PickPoint("Please pick a point to place group");
+
+                //Place the group
+                Transaction     trans       = new Transaction(doc);
+                trans.Start("Lab");
+                doc.Create.PlaceGroup(point, group.GroupType);
+                trans.Commit();
+            }
+            catch (Autodesk.Revit.Exceptions.ArgumentNullException ex)
+            {
                 return Result.Cancelled;
-
-            //Place the group
-            Transaction trans = new Transaction(doc);
-            trans.Start("Lab");
-            doc.Create.PlaceGroup(point, group.GroupType);
-            trans.Commit();
-
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException ex)
+            {
+                return Result.Cancelled;
+            }
             return Result.Succeeded;
         } // end of Execute
 
